b40a1df5fbcce7a46b71ca1e46d8667f2760f970
Viktor Bagaryan <vicgerrard@gmail.com>
2020-09-14 23:28:22 +0300

    make it works on iphone

--- src/App.js
+++ src/App.js
@@ -81,7 +81,7 @@ class App extends React.Component {
 						{
 							!!image &&
 							<Div style={{ textAlign: 'center' }}>
-                                <img src={ image } alt="remote file" referrerpolicy="no-referrer" />
+                                <img src={ image } alt="remote file" />
 							</Div>
 						}
                     </Group>
